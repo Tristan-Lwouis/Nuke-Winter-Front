90a929f9dfe6e114611e7e1fda98c3f50f0822da
Tristan Lwouis <regniertristan@gmail.com>
2026-01-19 14:49:40 +0100

    Connexion entre angular et spring pour la connexion d'un User (non sécurisé)

--- src/app/pages/log-in/log-in.ts
+++ src/app/pages/log-in/log-in.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { SnowEffect } from "../../components/snow-effect/snow-effect";
-import { NukeButton } from "../../components/nuke-button/nuke-button";
+import { SnowEffect } from '../../components/snow-effect/snow-effect';
+import { NukeButton } from '../../components/nuke-button/nuke-button';
 import { RouterLink } from '@angular/router';
 import { ParallaxDirective } from '../../shared/directives/parallax.directive';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -12,23 +12,21 @@ import { ApiService } from '../../core/services/api/api-service';
   templateUrl: './log-in.html',
   styleUrl: './log-in.scss',
 })
-
 export class LogIn {
+  loginForm = new FormGroup({
+    pseudo: new FormControl(),
+    password: new FormControl(),
+  });
 
-loginForm = new FormGroup({
-  pseudo: new FormControl(),
-  password: new FormControl()
-});
+  apiService = inject(ApiService);
+  constructor() {}
 
-apiService = inject(ApiService)
-constructor(){}
-
-onSubmit(){
-  this.apiService.post("log-in", {pseudo:this.loginForm.get("pseudo")})
-  .subscribe((data)=>{
-    console.log(data)
-  })
-  
+  onSubmit() {
+    console.log(this.loginForm.get('pseudo')?.value);
+    this.apiService
+      .post('log-in', { pseudo: this.loginForm.controls.pseudo.value })
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
-}
-
