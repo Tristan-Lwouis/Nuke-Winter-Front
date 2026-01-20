81419d107463a032a1bec5c4edaa79bca89c5056
Tristan Lwouis <regniertristan@gmail.com>
2026-01-20 13:40:42 +0100

    Implementation de la fonction qui permet dans choisir son avatar dans le game-config (non relier au back-end)

--- src/app/pages/game-config/game-config.ts
+++ src/app/pages/game-config/game-config.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SnowEffect } from "../../components/snow-effect/snow-effect";
 import { ParallaxDirective } from '../../shared/directives/parallax.directive';
 import { NukeButton } from "../../components/nuke-button/nuke-button";
+import { Avatar } from '../../core/models/avatar';
 
 @Component({
   selector: 'app-game-config',
@@ -11,4 +12,31 @@ import { NukeButton } from "../../components/nuke-button/nuke-button";
 })
 export class GameConfig {
 
+  currentAvatarIndex = 0;
+
+  avatars: Avatar[] = [
+    { id: 1, name: "SARAH", image: "/assets/images/avatar-sarah.png" },
+    { id: 2, name: "KURT", image: "/assets/images/avatar-kurt.png" },
+    { id: 3, name: "PEDRO", image: "/assets/images/avatar-pedro.png" }
+  ];
+
+  get currentAvatar(): Avatar {
+    return this.avatars[this.currentAvatarIndex];
+  }
+
+  /**
+   * 
+   * @param direction Permet de faire defiler tous les avatrs : 1 Avance dans le tableau, -1 recule dans le tableau
+   */
+  nextAvatar(direction: number) {
+    const len = this.avatars.length;
+
+    // Si liste recu est vide.
+    if (len === 0) {
+      console.warn("La liste des avatars est vide.");
+      return;
+    }
+    const step = direction > 0 ? 1 : direction < 0 ? -1 : 0;
+    this.currentAvatarIndex = (this.currentAvatarIndex + step + len) % len;
+  }
 }
