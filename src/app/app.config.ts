307122116811bf3a1e31236b2c5347f19f1aace6
tiny <tiny.lehartel@gmail.com>
2026-01-20 09:11:53 +0100

    correction erreur pesudo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { pesudoInterceptor } from './core/middleware/pseudo-interceptor';
+import { pseudoInterceptor } from './core/middleware/pseudo-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [// pour injecter les interceptors dans toutes les requetes
-    //provideHttpClient(withInterceptors([pesudoInterceptor])),
+    provideHttpClient(withInterceptors([pseudoInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
