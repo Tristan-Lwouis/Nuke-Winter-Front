23c83957095bd65c47d2a302f28a6ee671653cee
tiny <tiny.lehartel@gmail.com>
2026-01-21 22:21:40 +0100

    mise en place de la scene de type multi

---
+++ src/app/pages/scene-multi/scene-multi.ts
@@ -0,0 +1,84 @@
+import { ChangeDetectorRef, Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
+import { Scene } from '../../core/models/scene';
+import { SceneService } from '../../core/services/scene/scene-service';
+import { Response } from '../../core/models/response';
+
+@Component({
+  selector: 'app-scene-multi',
+  imports: [],
+  templateUrl: './scene-multi.html',
+  styleUrl: './scene-multi.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class SceneMulti implements OnInit {
+  selectedResponse: Response | undefined;
+  scene: Scene | undefined;
+
+  // Gestion de l'écriture de la description
+  displayedDescription = '';
+  speed = 60;
+  index = 0;
+
+  isQuestionResponseDisplayed = false;
+
+  sceneService = inject(SceneService);
+  cdr = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    //TODO: reflechir à la maniere dont on récupère la premiere scene
+    this.sceneService.read('s1s1').subscribe({
+      next: (scene: Scene) => {
+        if (scene) {
+          this.startScene(scene);
+        }
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  // nous permet de lancer une scene
+  startScene(scene: Scene) {
+    this.scene = scene;
+    this.displayedDescription = ''; 
+    this.isQuestionResponseDisplayed = false;
+    this.index = 0;
+    this.typeWriter();
+  }
+
+  skipDescription() {
+    this.displayedDescription = this.scene!.description;
+    this.isQuestionResponseDisplayed = true;
+  }
+
+  selectResponse(response: Response) {
+    this.startScene(response.nextScene);
+  }
+
+  // pour ecrire la description en mode lettre par lettre
+  private typeWriter() {
+    if (!this.scene) return;
+
+    const description = this.scene.description;
+
+    if (this.index < description.length) {
+      this.displayedDescription += description.charAt(this.index);
+      this.index++;
+
+      this.cdr.markForCheck();
+
+      // si la description est affiché on ne continue pas
+      if (!this.isQuestionResponseDisplayed) {
+        setTimeout(() => this.typeWriter(), this.speed);
+      }
+    } else {
+      //execute cette methode après avoir écris tous les caracteres pour dire d'affciher la question et les reposnes
+      this.onTypingFinished();
+    }
+  }
+
+  // quand l''écriture de la déscription est terminé on affiche la question et les reponses
+  onTypingFinished() {
+    this.isQuestionResponseDisplayed = true;
+    this.cdr.markForCheck();
+  }
+}
