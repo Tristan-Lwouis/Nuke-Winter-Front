91b206e5857673d1ebc2edccfdb2448225d740e4
Tristan Lwouis <regniertristan@gmail.com>
2026-01-21 08:15:35 +0100

    Création du model et du service pour avatar et scenario + Finalisation de la page game config + Modification des intéractions curseurs

--- src/app/pages/game-config/game-config.ts
+++ src/app/pages/game-config/game-config.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { SnowEffect } from "../../components/snow-effect/snow-effect";
+import { Component, inject, OnInit } from '@angular/core';
+import { SnowEffect } from '../../components/snow-effect/snow-effect';
 import { ParallaxDirective } from '../../shared/directives/parallax.directive';
-import { NukeButton } from "../../components/nuke-button/nuke-button";
+import { NukeButton } from '../../components/nuke-button/nuke-button';
 import { Avatar } from '../../core/models/avatar';
+import { AvatarService } from '../../core/services/avatar/avatar-service';
+import { ScenarioService } from '../../core/services/scenario/scenario-service';
+import { Scenario } from '../../core/models/scenario';
 
 @Component({
   selector: 'app-game-config',
@@ -10,30 +13,54 @@ import { Avatar } from '../../core/models/avatar';
   templateUrl: './game-config.html',
   styleUrl: './game-config.scss',
 })
-export class GameConfig {
+export class GameConfig implements OnInit {
+  avatarService = inject(AvatarService);
+  scenarioService = inject(ScenarioService);
 
   currentAvatarIndex = 0;
 
-  avatars: Avatar[] = [
-    { id: 1, name: "SARAH", image: "/assets/images/avatar-sarah.png" },
-    { id: 2, name: "KURT", image: "/assets/images/avatar-kurt.png" },
-    { id: 3, name: "PEDRO", image: "/assets/images/avatar-pedro.png" }
-  ];
+  avatars: Avatar[] = [{ id: 2, name: 'KURT', image: '/assets/images/avatar-kurt.webp' },];
+  scenarios: Scenario[] = [];
 
-  get currentAvatar(): Avatar {
+  // Au chargement du composant je récupère les listes d'avatars et de scénarios
+  ngOnInit(): void {
+    // ==== Récupère la liste des avatars ====
+    this.avatarService.getAllAvatars().subscribe({
+      next: (data: Avatar[]) => {
+        console.log('Avatars récupérés :', data);
+        this.avatars = data;
+      },
+    });
+    // ==== Récupère la liste des scénarios ====
+    this.scenarioService.getAllScenarios().subscribe({
+      next: (data: Scenario[]) => {
+        console.log('Scénarios récupérés :', data);
+        this.scenarios = data;
+      },
+    });
+  }
+
+  // avatars: Avatar[] = [
+  //   { id: 1, name: 'SARAH', image: '/assets/images/avatar-sarah.png' },
+  //   { id: 2, name: 'KURT', image: '/assets/images/avatar-kurt.png' },
+  //   { id: 3, name: 'PEDRO', image: '/assets/images/avatar-pedro.png' },
+  // ];
+
+  get currentAvatar(): Avatar | undefined {
     return this.avatars[this.currentAvatarIndex];
   }
 
   /**
-   * 
-   * @param direction Permet de faire defiler tous les avatrs : 1 Avance dans le tableau, -1 recule dans le tableau
+   * @param direction Permet de faire defiler tous les avatrs
+   *    1 Avance dans le tableau
+   *   -1 recule dans le tableau
    */
   nextAvatar(direction: number) {
     const len = this.avatars.length;
 
     // Si liste recu est vide.
     if (len === 0) {
-      console.warn("La liste des avatars est vide.");
+      console.warn('La liste des avatars est vide.');
       return;
     }
     const step = direction > 0 ? 1 : direction < 0 ? -1 : 0;
