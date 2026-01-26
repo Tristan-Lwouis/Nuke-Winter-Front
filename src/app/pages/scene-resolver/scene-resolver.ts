aaefcd7e400be87d3f0d59265bd81084e4478422
Auriane Mignon <auriane.mignon@free.fr>
2026-01-26 18:21:56 +0100

    html resolver

---
+++ src/app/pages/scene-resolver/scene-resolver.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scene-resolver',
+  imports: [],
+  templateUrl: './scene-resolver.html',
+  styleUrl: './scene-resolver.scss',
+})
+export class SceneResolver {
+
+}
